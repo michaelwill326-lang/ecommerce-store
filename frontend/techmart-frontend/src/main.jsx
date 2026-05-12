64eb97353a716025be7b1ebc66001da70925c58c
Michael Will <michaelwill326@gmail.com>
2026-05-12 16:18:25 -0600

    Fix frontend routing and product pages

--- frontend/techmart-frontend/src/main.jsx
+++ frontend/techmart-frontend/src/main.jsx
@@ -1,17 +1,10 @@
-// src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
