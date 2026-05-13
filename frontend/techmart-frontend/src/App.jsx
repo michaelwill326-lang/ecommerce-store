4e28428612809fb419724d8075cd221d32dbd4ca
Michael Will <michaelwill326@gmail.com>
2026-05-13 07:55:52 -0600

    Fix React crash completely

--- frontend/techmart-frontend/src/App.jsx
+++ frontend/techmart-frontend/src/App.jsx
@@ -1,25 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import ProductDetail from "./pages/ProductDetail";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Cart from "./pages/Cart";
-import Navbar from "./components/Navbar";
-import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
