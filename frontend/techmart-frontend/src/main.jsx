75577b0caae09cd544af823713cbcf472bd0bc53
Michael Will <michaelwill326@gmail.com>
2026-05-26 19:48:20 -0600

    fix: wrap main app execution context in BrowserRouter

--- frontend/techmart-frontend/src/main.jsx
+++ frontend/techmart-frontend/src/main.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { CartProvider } from "./context/CartContext";
-import { WishlistProvider } from "./context/WishlistContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css'; // Or whatever your main CSS file is named
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>
-      <WishlistProvider>
-        <App />
-      </WishlistProvider>
-    </CartProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
