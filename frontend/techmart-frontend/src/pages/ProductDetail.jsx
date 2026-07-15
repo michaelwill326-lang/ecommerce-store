c2d78018bcc3cb34bbed1ddd6debe225d13579b3
Michael Will <michaelwill326@gmail.com>
2026-07-15 04:09:30 -0600

    fix: add missing ProductDetailSkeleton import causing iOS crash

--- frontend/techmart-frontend/src/pages/ProductDetail.jsx
+++ frontend/techmart-frontend/src/pages/ProductDetail.jsx
@@ -1,3 +1,4 @@
+import { ProductDetailSkeleton } from "../components/Skeleton";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useContext } from "react";
